a31ce174881797edb43d89da3b2cd5e5eba6a76a
Murilo Ferreira <152446247+muriloferreira23@users.noreply.github.com>
2026-07-29 05:22:03 -0300

    commit

--- javascript/contato.js
+++ javascript/contato.js
@@ -1,7 +1,23 @@
 //===================
+// Batalha do Menu
+//===================
+const botao = document.getElementById("menuindx");
+const menu = document.getElementById("omenu");
+
+botao.addEventListener("click", function(){
+
+    if(menu.style.display === "flex"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "flex";
+    }
+
+});
+
+
+//===================
 // Botão Contato
 //===================
-
 
 const formulario = document.querySelector("form");
 
