be55d4776566a22a07919270741737bfe180e81c
Murilo Ferreira <152446247+muriloferreira23@users.noreply.github.com>
2026-07-29 04:56:30 -0300

    commit

---
+++ javascript/contato.js
@@ -0,0 +1,17 @@
+//===================
+// Botão Contato
+//===================
+
+
+const formulario = document.querySelector("form");
+
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    alert("Mensagem enviada para o SAC da Escadona");
+
+});
+
+//===================
+// Algo ruim será colocado aqui
+//===================
---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+//===================
+// Batalha do Menu
+//===================
+const botao = document.getElementById("menuindx");
+const menu = document.getElementById("omenu");
+
+botao.addEventListener("click", function(){
+
+    if(menu.style.display === "flex"){
+        menu.style.display = "none";
+    }else{
+        menu.style.display = "flex";
+    }
+
+});
+
+//===================
+// Vídeo 
+//===================
+const video = document.getElementById("notebook");
