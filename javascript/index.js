1b91181810330fe16b88a91a1670d36112daad69
Carlos Antônio de Jesus Rocha <perolanegrah05@gmail.com>
2026-07-30 03:48:41 -0300

    commit

--- javascript/index.js
+++ javascript/index.js
@@ -4,24 +4,12 @@
 const botao = document.getElementById("menuindx");
 const menu = document.getElementById("omenu");
 
-// Abrir/fechar essa dsgraça de botão do caralho
-
-  botao.addEventListener('click', function() {
-    menu.classList.toggle('aberto'); // Disgarça
-
-});  
-
-// ===================================
-// Fechar o menu e abrir o cu
-// ===================================
-
-document.addEventListener('click', function(evento) {
-    if (!menu.contains(evento.target) && !botao.contains(evento.target)) {
-        menu.classList.remove("aberto");
-    }
-
+// =====================================
+// Abrir e fechar o menu/função satanica
+// =====================================
+botao.addEventListener('click', function() {
+    menu.classList.toggle('aberto');
 });
-
 //===================
 // Vídeo 
 //===================
