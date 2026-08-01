ff0c26e62249997519dfb462e1ea430eeba134c5
Murilo Ferreira <152446247+muriloferreira23@users.noreply.github.com>
2026-08-01 02:32:22 -0300

    Co-authored-by: Carlos Antônio de Jesus Rocha <antoonyz@users.noreply.github.com>

--- javascript/index.js
+++ javascript/index.js
@@ -5,17 +5,7 @@ const botao = document.getElementById("menuindx");
 const menu = document.getElementById("omenu");
 
 // =====================================
-// Abrir e fechar o menu/função satanica
+// Header disgraça
 // =====================================
-botao.addEventListener('click', function() {
-    menu.classList.toggle('aberto');
-});
-//===================
-// Vídeo 
-//===================
-const video = document.getElementById("notebook");
-
-
-
 
 // O DIABO NÃO VERÁ ISSO - alguem corrijirá
