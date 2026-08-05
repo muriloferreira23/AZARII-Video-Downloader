be0cd5d88da6bc998f579397fd8966a56c099267
Carlos Antônio de Jesus Rocha <perolanegrah05@gmail.com>
2026-08-05 02:39:44 -0300

    commit

--- javascript/contato.js
+++ javascript/contato.js
@@ -1,12 +1,31 @@
+//===================
+// SAC Escadona
+//===================
 const formulario = document.querySelector("form");
 
 formulario.addEventListener("submit", function(event){
     event.preventDefault();
 
     alert("Mensagem enviada para o SAC da Escadona");
-
+    
+    formulario.reset();
 });
 
+//===================
+// Botão FAQ
+//===================
+
+const botoesFaq = document.querySelectorAll(".botao-faq");
+    botoesFaq.forEach((botao) => {
+        botao.addEventListener("click", function(){
+        
+        const resposta = this.nextElementSibling;
+
+        resposta.classList.toggle("ativa");
+
+    });
+
+});
 //===================
 // Algo ruim será colocado aqui
 //===================
